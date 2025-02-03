8e4d8aae1e05a359aae7515179da9864957ff893
Kendall <kendamv@gmai.com>
2025-02-03 18:46:45 +0000

    arreglo del .py

--- cuadratura/__init__.py
+++ cuadratura/__init__.py
@@ -1,3 +1,3 @@
 """Calcula cualquier integral mediante el método de cuadraturas gaussianas.
 Gracias a la librería numpy y sus diferentes usos como sum, sin, max, copy, etc..., se 
-pueden calcular fàcilmente las funciones necesarias!"""
+pueden calcular fàcilmente las funciones necesarias!."""
